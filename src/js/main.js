f10e74e6824c5f67a4fa4fa1fec8b747800e454b
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-08-13 23:29:51 +0200

    add forms.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import modals from "./modules/modal";
 import slides from "./modules/slides";
+import forms from "./modules/forms";
 
 window.addEventListener('click', () => {
 "use strict";
 modals();
 slides('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 slides('.main-slider-item', 'vertical');
+forms();
 
 });
