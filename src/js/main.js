8cef449993d48eadf399c0a2ed508389a29833fa
Богдан Атаманюк <bogdanatamanuk@MacBook-Air-Bogdan.local>
2022-08-14 00:38:57 +0200

    add mask for phone

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,18 @@
 import modals from "./modules/modal";
 import slides from "./modules/slides";
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
+
 
 window.addEventListener('click', () => {
-"use strict";
 modals();
 slides('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 slides('.main-slider-item', 'vertical');
 forms();
+mask('[name="phone"]');
+checkTextInputs('[name="name"]');
+checkTextInputs('[name="message"]');
+
 
 });
